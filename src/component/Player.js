cbf2901faf65f8407d911e383550515a64fe062c
isaac <oseasisaac91@gmail.com>
2020-03-13 14:45:08 -0400

    one coordinate click per turn and page resets after a player wins

--- src/component/Player.js
+++ src/component/Player.js
@@ -7,14 +7,40 @@ function Player(props) {
 
   const [state, dispatch] = useContext(Context);
 
+  function checkAllBoatCoordinatesFilled(state, player){
+    if(state.turn < 3){
+      if(player === 'Player 1'){
+        let array = []
+        state.playerOneBoats.forEach(function(boat){
+            array = [...array, ...Object.values(boat)[0]]
+        })
+        return !array.includes(0)
+      } else {
+        let array = []
+        state.playerTwoBoats.forEach(function(boat){
+            array = [...array, ...Object.values(boat)[0]]
+        })
+        return !array.includes(0)
+      }
+    }
+  }
+
   return(
     <div>
       <h1>{props.player}'s Turn</h1>
-      <button onClick={() => dispatch({type: 'TURN_COUNTER'})}>
+      {checkAllBoatCoordinatesFilled(state, props.player) ?
+        <button onClick={() => dispatch({type: 'TURN_COUNTER'})}>
         Finalize Boat Placement
-      </button>
-      <Grid />
-      <BoatContainer  />
+        </button>
+        :
+        <div></div>
+      }
+      <div style={{display: 'flex'}}>
+        <div>
+          <Grid />
+        </div>
+        <BoatContainer  />
+      </div>
     </div>
   )
 }
